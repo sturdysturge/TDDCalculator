14421e4c6967ac0053c9c1d87541d632085522e3
robsturgeon <github@sturdysturge.com>
2021-11-06 20:07:05 +0000

    Initial commit

---
+++ TDDCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDDCalculator
+//
+//  Created by Rob Sturgeon on 06/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TDDCalculator/TDDCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDCalculatorApp.swift
+//  TDDCalculator
+//
+//  Created by Rob Sturgeon on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TDDCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
